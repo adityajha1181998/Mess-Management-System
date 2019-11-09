dde84e2cf1038a55ed0c94236a33db37b6fa12cf
Lokesh Nirania <lknmahan@gmail.com>
2019-11-10 04:50:26 +0530

    model template initialised

--- mess_project/mess_app/models.py
+++ mess_project/mess_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class mess(models.Model):
+    mess_id = models.IntegerField()
+    mess_name = models.CharField('Mess Name',max_length=50,blank=False)
+    phone = models.CharField('Mess Contact',max_length=15)
+    capacity = models.IntegerField()
+
+# The above statement is equivalent to 
+    # CREATE TABLE mess (
+    #     mess_id int,
+    #     mess_name varchar(50) not null,
+    #     phone varchar(15),
+    #     capacity int
+    # )
