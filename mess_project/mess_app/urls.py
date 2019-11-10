2e761a01dd9e5b9cd8bc3d073ef415946e5f7373
Lokesh Nirania <lknmahan@gmail.com>
2019-11-10 05:43:09 +0530

    Mess Manager html template started

--- mess_project/mess_app/urls.py
+++ mess_project/mess_app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.manager_home, name='manager_home'),
 ]
--- mess_project/mess_app/views.py
+++ mess_project/mess_app/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def manager_home(request):
+    return render(request,'mess_app/mess.html')
