b4b6c0e0fb7c64dcd0e4f7e0606e05b8b2410003
adityajha1181998 <44377261+adityajha1181998@users.noreply.github.com>
2019-11-10 11:03:36 +0530

    Update models.py

--- mess_project/mess_app/models.py
+++ mess_project/mess_app/models.py
@@ -9,7 +9,7 @@ class mess(models.Model):
     phone = models.CharField('Mess Contact',max_length=15)
     capacity = models.IntegerField()
 
-# The above statement is equivalent to 
+# The above statement is equivalent to
     # CREATE TABLE mess (
     #     mess_id int,
     #     mess_name varchar(50) not null,
@@ -40,6 +40,8 @@ class inventory(models.Model):
     item_id = models.IntegerField()
     mess_id = models.IntegerField()
     quantity = models.IntegerField()
+    item = models.CharField(max_length=30)
+
 
 class consumption(models.Model):
     mess_id = models.IntegerField()
@@ -66,3 +68,48 @@ class bills(models.Model):
 class expenses(models.Model):
     date = models.DateTimeField()
     amount = models.IntegerField()
+
+class mess_menu(models.Model):
+    day = models.CharField(max_length=10)
+    items = models.TextField()
+    menu_id = models.IntegerField()
+class serves(models.Model):
+    menu_id = models.IntegerField()
+    mess_id = models.IntegerField()
+    meal_type = models.TextField()
+class guest(model.Model):
+    guest_id = models.CharField(max_length=10)
+    name = models.CharField('Guest Name',max_length=50)
+    phone = models.CharField('Guest Contact',max_length=20)
+class eats(mosels.Model):
+    guest_id = models.CharField(max_length=10)
+    payment = models.IntegerField()
+    meal_type = models.TextField()
+    mess_id = models.IntegerField()
+class mess_council(models.Model):
+    council_id = models.IntegerField()
+    mess_id = models.IntegerField()
+    head = models.CharField(max_length=20)
+class member(models.Model):
+    s_id =  models.IntegerField()
+    member_id = models.CharField('Mess member_id',max_length=10)
+    council_id = models.IntegerField()
+    joining_date = models.DateTimeField()
+class registers(models.Model):
+    s_id =  models.IntegerField()
+    mess_id = models.IntegerField()
+    semester = models.CharField('Semester',max_length=10)
+    payment = models.IntegerField()
+    joining_date = models.DateTimeField()
+class student(models.Model):
+    s_id =  models.IntegerField()
+    s_name =  models.CharField('Student_Name',max_length=50)
+    email_id = EmailField(max_length = 254)
+    phone = models.CharField('Student Contact',max_length=20)
+class feedback(models.Model):
+    feedback_id = models.IntegerField()
+    s_id =  models.IntegerField()
+    council_id = models.IntegerField()
+    complaint = models.TextField()
+    status = models.CharField(max_length=6)
+    date = models.DateTimeField()
